eb2c1e12b1db7840d1d316661e7a40c1e990070f
nbr41to <nbr.41to@gmail.com>
2021-02-08 22:55:47 +0900

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,10 @@
+import { useRouter } from 'next/router'
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  const router = useRouter()
+  setTimeout(() => {
+    router.push('/')
+  }, 2000)
+
+  return <div>解散されました。トップに戻ります。</div>
 }
