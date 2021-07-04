49185b01fd97402e17b21910ca983e05bb00ad6f
Chisato Kato <i.like.nuko@gmail.com>
2021-07-04 15:33:51 +0900

    errorの修正をしました。

--- src/components/displays/OnGame.tsx
+++ src/components/displays/OnGame.tsx
@@ -1,45 +1,60 @@
-import { firebase } from 'src/firebase'
-import React from 'react'
-import { ContinueSelect } from '../templates/ContinueSelect'
-import { ReadyCheck } from '../templates/ReadyCheck'
-import { useRecoilState } from 'recoil'
-import { room, user } from 'src/recoil/atom'
-import { GameBoard } from '../templates/GameBoard'
+import { firebase } from 'src/firebase';
+import React from 'react';
+import { ContinueSelect } from '../templates/ContinueSelect';
+import { ReadyCheck } from '../templates/ReadyCheck';
+import { useRecoilState } from 'recoil';
+import { room, user } from 'src/recoil/atom';
+import { GameBoard } from '../templates/GameBoard';
 
 export const OnGame = () => {
-  const [userInfo, setUserInfo] = useRecoilState(user)
-  const [roomInfo, setRoomInfo] = useRecoilState(room)
+  const [userInfo, setUserInfo] = useRecoilState(user);
+  const [roomInfo, setRoomInfo] = useRecoilState(room);
 
   // WolfとThemeの割り振りとゲーム中への切り替え
   const gameStart = async () => {
-    const themeDice = Math.floor(Math.random() * 2)
-    const wolfDice = Math.floor(Math.random() * Object.keys(roomInfo.member).length)
-    const wolfId = Object.keys(roomInfo.member)[wolfDice]
-    Object.keys(roomInfo.member).map(id => {
+    const themeDice = Math.floor(Math.random() * 2);
+    const wolfDice = Math.floor(
+      Math.random() * Object.keys(roomInfo.member).length
+    );
+    const wolfId = Object.keys(roomInfo.member)[wolfDice];
+    Object.keys(roomInfo.member).map((id) => {
       if (id === wolfId) {
         // wolf theme in
-        firebase.firestore().collection('rooms').doc(roomInfo.roomId).update({
-          [`member.${id}.theme`]: roomInfo.theme[themeDice]
-        })
+        firebase
+          .firestore()
+          .collection('rooms')
+          .doc(roomInfo.roomId)
+          .update({
+            [`member.${id}.theme`]: roomInfo.theme[themeDice],
+          });
         // console.log(id + ':' + roomInfo.theme[themeDice])
       } else {
         // no wolf theme in
-        firebase.firestore().collection('rooms').doc(roomInfo.roomId).update({
-          [`member.${id}.theme`]: roomInfo.theme[themeDice === 0 ? 1 : 0]
-        })
+        firebase
+          .firestore()
+          .collection('rooms')
+          .doc(roomInfo.roomId)
+          .update({
+            [`member.${id}.theme`]: roomInfo.theme[themeDice === 0 ? 1 : 0],
+          });
         // console.log(id + ':' + roomInfo.theme[themeDice === 0 ? 1 : 0])
       }
-    })
-    await firebase.firestore().collection('rooms').doc(roomInfo.roomId).update({
-      isGaming: true,
-    }).then(() => console.log('gamingOn'))
-  }
+    });
+    await firebase
+      .firestore()
+      .collection('rooms')
+      .doc(roomInfo.roomId)
+      .update({
+        isGaming: true,
+      })
+      .then(() => console.log('gamingOn'));
+  };
 
   return (
     <div>
       <ReadyCheck gameStart={gameStart} />
       {roomInfo?.isGaming && <GameBoard />}
-      {roomInfo?.finished && <ContinueSelect gameStart={gameStart} />}
+      {roomInfo?.finished && <ContinueSelect />}
     </div>
-  )
-}
+  );
+};
